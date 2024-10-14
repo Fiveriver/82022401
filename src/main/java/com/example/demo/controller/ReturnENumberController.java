a2e0b72a292947a49f33ec64e4afa751837b26b0
kangminoh <minable28@gmail.com>
2024-10-14 15:19:00 +0900

    리포지토리 마이너 수정 및 사번리턴 API 개발

---
+++ src/main/java/com/example/demo/controller/ReturnENumberController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class ReturnENumberController {
+
+    @GetMapping("/api/usernumber")
+    public Map<String, String> getUserId() {
+
+        Map<String, String> response = new HashMap<>();
+            response.put("userNumber", "82022401");
+        return response;
+
+    }
+}
